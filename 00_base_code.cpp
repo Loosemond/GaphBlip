27200a55fa44f3e0cfe1ca23edbc43d7acde5cc7
Loosemond <joao--ribeiro@hotmail.com>
2022-05-12 23:33:28 +0100

    queue families

--- 00_base_code.cpp
+++ 00_base_code.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <cstring>
 #include <map>
+#include <optional>
 
 // TODO Move window to core window and make it compile
 // TODO Advance with the Messages call back
@@ -208,7 +209,48 @@ private:
             return 0;
         }
 
+        if (!findQueueFamilies(device).isComplete())
+        {
+            return 0;
+        }
+
         return score;
+    }
+
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete()
+        {
+            return graphicsFamily.has_value();
+        }
+    };
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+        // Logic to find queue family indices to populate struct with
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto &queueFamily : queueFamilies)
+        {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            {
+                indices.graphicsFamily = i;
+            }
+            if (indices.isComplete())
+            {
+                break;
+            }
+            i++;
+        }
+        return indices;
     }
 };
 
